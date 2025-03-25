fad29018b11e933d652eb378c402aa6ff6641753
Zoltán Sóki <zsoki@chemaxon.com>
2025-03-25 13:58:46 +0100

    Create matches

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"name-clash/graph"
 	"name-clash/names"
 )
 
 func main() {
 	nameNodes, nameEdges := names.CreateNameGraph("resources/noi.txt", "Anna", 1)
-	matchups := names.CreateRandomMatchups(nameNodes)
-	println(matchups)
+	tournament := names.CreateTournament(nameNodes)
+	fmt.Printf("Tournament: %v\n", tournament)
+	tournament = names.Vote(tournament, 0, 0)
+	tournament = names.Vote(tournament, 0, 1)
 	graph.ExportGraphML(nameNodes, nameEdges)
 }
--- names/game.go
+++ names/game.go
@@ -2,7 +2,13 @@ package names
 
 import "math/rand"
 
-func CreateRandomMatchups(names []*Name) [][2]*Name {
+type Tournament struct {
+	Remaining [][2]*Name
+	Winners   []*Name
+	Losers    []*Name
+}
+
+func CreateTournament(names []*Name) Tournament {
 	// TODO what to do if not even?
 	if len(names)%2 != 0 {
 		names = names[1:]
@@ -10,9 +16,27 @@ func CreateRandomMatchups(names []*Name) [][2]*Name {
 
 	rand.Shuffle(len(names), func(i, j int) { names[i], names[j] = names[j], names[i] })
 
-	matchups := make([][2]*Name, len(names)/2)
+	matches := make([][2]*Name, len(names)/2)
 	for i := 0; i < len(names); i += 2 {
-		matchups[i/2] = [2]*Name{names[i], names[i+1]}
+		matches[i/2] = [2]*Name{names[i], names[i+1]}
 	}
-	return matchups
+
+	return Tournament{Remaining: matches}
+}
+
+func Vote(tournament Tournament, matchIdx, winnerIdx int) Tournament {
+	loserIdx := 1 - winnerIdx
+	winner := tournament.Remaining[matchIdx][winnerIdx]
+	loser := tournament.Remaining[matchIdx][loserIdx]
+
+	winner.Wins += 1
+	loser.Wins -= 1
+
+	tournament.Winners = append(tournament.Winners, winner)
+	tournament.Losers = append(tournament.Losers, loser)
+
+	// TODO handle winners/losers bracket?
+
+	tournament.Remaining = append(tournament.Remaining[:matchIdx], tournament.Remaining[matchIdx+1:]...)
+	return tournament
 }
